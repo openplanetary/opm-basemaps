fb03c8747d1f21e4548877bdfe3a2db1a2700fd3
Nicolas Manaud <nmanaud@gmail.com>
2019-06-17 10:46:04 -0700

    Updates

---
+++ static-map-images/generate.sh
@@ -0,0 +1,9 @@
+carto-print opmbuilder opm-mars-basemap-v0-2 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mars-colour-celestia 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mars-colour-mola-elevation 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mars-hillshade 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mars-shaded-colour-mola-elevation 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mars-shaded-grayscale-mola-elevation 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mars-viking-mdim21 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-moon-basemap-v0-1 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-moon-hillshaded-albedo 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
