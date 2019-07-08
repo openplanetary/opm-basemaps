fa40a5a06aa4706ff0d9b88fe9656503d6558abe
Nicolas Manaud <nmanaud@gmail.com>
2019-07-08 16:09:23 +0200

    Added first draft of Mercury basemap (Angelo)

--- static-map-images/generate.sh
+++ static-map-images/generate.sh
@@ -7,3 +7,4 @@ carto-print opmbuilder opm-mars-shaded-grayscale-mola-elevation 40 20 2 " -40.0,
 carto-print opmbuilder opm-mars-viking-mdim21 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
 carto-print opmbuilder opm-moon-basemap-v0-1 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
 carto-print opmbuilder opm-moon-hillshaded-albedo 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
+carto-print opmbuilder opm-mercury-basemap-v0-1 40 20 2 " -40.0,-20.0,40.0,20.0" --dpi 72 --output-dir .
